24c22a079cc275136d8b241669f2bbeae14bcc58
Mariia <90716920+Mariia-Petlovana-91@users.noreply.github.com>
2024-12-03 23:08:57 +0100

    init commit

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,14 @@
-const generateContacts = async (number) => {};
+import { createFakeContact } from "../utils/createFakeContact.js";
 
-generateContacts(5);
+const generateContacts = async (number) => {
+	// const arr = [];
+	// for (let index = 0; index < number; index += 1) {
+	// 	let contact = createFakeContact();
+	// 	arr.push(contact);
+	// }
+
+	const newContacts = Array(number).fill({}).map(createFakeContact);
+	console.log(newContacts);
+};
+
+generateContacts(2);
